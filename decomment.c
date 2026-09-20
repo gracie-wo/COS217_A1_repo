5924501725a75680082e3b9c709892cd45f63b72
Grace Hua <gh0795@princeton.edu>
2026-09-20 00:49:11 -0400

    intial code (start)

---
+++ decomment.c
@@ -0,0 +1,150 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+enum Statetype {NOT_COMMENT, SLASH, SLASH_STAR, SLASH_STAR_STAR, COMMENT, STRING, BACKSLASH, CHAR};
+
+enum Statetype handleNotCommentState(int c){
+    if(c == '/'){
+        return SLASH;
+    } else if(c == '"'){
+        putchar(c);
+        return STRING;
+    } else if(c == '\''){
+        putchar(c);
+        return CHAR;
+    } else {
+        putchar(c);
+        return NOT_COMMENT;
+    }
+}
+
+enum Statetype handleSlashState(int c){
+    if(c == '*'){
+        putchar(' ');
+        return SLASH_STAR;
+    } else {
+        putchar('/');
+        putchar(c);
+        return NOT_COMMENT;
+    }
+}
+
+enum Statetype handleSlashStarState(int c){
+    if(c == '*'){
+        return SLASH_STAR_STAR;
+    } else if(c == '\n'){
+        putchar(c);
+        return SLASH_STAR;
+    } else {
+        return SLASH_STAR;
+    }
+}
+
+enum Statetype handleSlashStarStarState(int c){
+    if(c == '/'){
+        return COMMENT;
+    } else if(c == '*'){
+        return SLASH_STAR_STAR;
+    } else {
+        return SLASH_STAR;
+    }
+}
+
+enum Statetype handleCommentState(int c){
+    putchar(c);
+    return NOT_COMMENT;
+}
+
+enum Statetype handleStringState(int c){
+    if(c == '"'){
+        putchar(c);
+        return NOT_COMMENT;
+    } else if (c == '\n'){
+        putchar(c);
+        return STRING;
+    }
+    else if(c == '\\'){
+        putchar(c);
+        return BACKSLASH;
+    } else {
+        putchar(c);
+        return STRING;
+    }
+}
+
+enum Statetype handleBackslashState(int c){
+    putchar(c);
+    return STRING;
+}
+
+enum Statetype handleCharState(int c){
+    if(c == '\''){
+        putchar(c);
+        return NOT_COMMENT;
+    } else if (c == '\n'){
+        putchar(c);
+        return CHAR;
+    }
+    else if(c == '\\'){
+        putchar(c);
+        return BACKSLASH;
+    } else {
+        putchar(c);
+        return CHAR;
+    }
+}
+
+int main(void){
+    int c;
+    int LINE_START = 1; 
+    int linenum = LINE_START;
+    int status = EXIT_SUCCESS;
+    enum Statetype state = NOT_COMMENT;
+
+    while((c = getchar()) != EOF){
+        if(c == '\n'){
+            linenum++;
+        }
+
+        switch(state){
+            case NOT_COMMENT:
+                state = handleNotCommentState(c);
+                status = EXIT_SUCCESS;
+                break;
+            case SLASH:
+                state = handleSlashState(c);
+                status = EXIT_SUCCESS;
+                break;
+            case SLASH_STAR:
+                state = handleSlashStarState(c);
+                status = EXIT_FAILURE;
+                break;
+            case SLASH_STAR_STAR:
+                state = handleSlashStarStarState(c);
+                status = EXIT_FAILURE;
+                break;
+            case COMMENT:
+                state = handleCommentState(c);
+                status = EXIT_SUCCESS;
+                break;
+            case STRING:
+                state = handleStringState(c);
+                status = EXIT_SUCCESS;
+                break;
+            case BACKSLASH:
+                state = handleBackslashState(c);
+                status = EXIT_SUCCESS;
+                break;
+            case CHAR:
+                state = handleCharState(c);
+                status = EXIT_SUCCESS;
+                break;
+        }
+    } 
+    
+    if(status == EXIT_FAILURE){
+        printf("Error: lines %d: unterminated comment\n", linenum);
+    }
+
+    return status;
+}
