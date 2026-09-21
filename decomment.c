0741f39353dfe0317d7ea1bec2a29d44adcb0cfe
Grace Hua <gh0795@princeton.edu>
2026-09-21 15:20:46 -0400

    put error message in stderr

--- decomment.c
+++ decomment.c
@@ -145,7 +145,7 @@ int main(void){
     } 
     
     if(status == EXIT_FAILURE){
-        printf("Error: lines %d: unterminated comment\n", linenum);
+        fprintf(stderr, "Error: lines %d: unterminated comment\n", linenum);
     }
 
     return status;
