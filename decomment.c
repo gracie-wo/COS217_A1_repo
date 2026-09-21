816f5291f6900c30993aac363266065ce1554e0d
Grace Hua <gh0795@princeton.edu>
2026-09-21 15:25:09 -0400

    get ride of \n

--- decomment.c
+++ decomment.c
@@ -145,7 +145,7 @@ int main(void){
     } 
     
     if(status == EXIT_FAILURE){
-        fprintf(stderr, "Error: lines %d: unterminated comment\n", linenum);
+        fprintf(stderr, "Error: lines %d: unterminated comment", linenum);
     }
 
     return status;
