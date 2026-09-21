4e1824911ba0b02ba24bba6bfd8b60945b51302a
Grace Hua <gh0795@princeton.edu>
2026-09-21 19:37:25 -0400

    added meaningful names

--- decomment.c
+++ decomment.c
@@ -1,102 +1,102 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <stdlib.h>
-enum Statetype {NOT_COMMENT, SLASH, SLASH_STAR, SLASH_STAR_STAR, STRING, BACKSLASH_STRING, BACKSLASH_CHAR, CHAR};
+enum Statetype {NOT_IN_COMMENT, SLASH, IN_COMMENT, IN_COMMENT_STAR, IN_STRING, ESCAPE_IN_STRING, ESCAPE_IN_CHAR, IN_CHAR};
 
-enum Statetype handleNotCommentState(int c){
+enum Statetype handleNotInCommentState(int c){
     if(c == '/'){
         return SLASH;
     } else if(c == '"'){
         putchar(c);
-        return STRING;
+        return IN_STRING;
     } else if(c == '\''){
         putchar(c);
-        return CHAR;
+        return IN_CHAR;
     } else {
         putchar(c);
-        return NOT_COMMENT;
+        return NOT_IN_COMMENT;
     }
 }
 
 enum Statetype handleSlashState(int c){
     if(c == '*'){
         putchar(' ');
-        return SLASH_STAR;
+        return IN_COMMENT;
     } else if(c == '/'){
         putchar(c);
         return SLASH;
     } else if(c == '"'){
         putchar('/');
         putchar(c);
-        return STRING;
+        return IN_STRING;
     } else if(c == '\''){
         putchar('/');
         putchar(c);
-        return CHAR;
+        return IN_CHAR;
     } else {
         putchar('/');
         putchar(c);
-        return NOT_COMMENT;
+        return NOT_IN_COMMENT;
     }
 }
 
-enum Statetype handleSlashStarState(int c){
+enum Statetype handleInCommentState(int c){
     if(c == '*'){
-        return SLASH_STAR_STAR;
+        return IN_COMMENT_STAR;
     } else if(c == '\n'){
         putchar(c);
-        return SLASH_STAR;
+        return IN_COMMENT;
     } else {
-        return SLASH_STAR;
+        return IN_COMMENT;
     }
 }
 
-enum Statetype handleSlashStarStarState(int c){
+enum Statetype handleInCommentStarState(int c){
     if(c == '/'){
-        return NOT_COMMENT;
+        return NOT_IN_COMMENT;
     } else if(c == '*'){
-        return SLASH_STAR_STAR;
+        return IN_COMMENT_STAR;
     } else if(c == '\n'){
         putchar(c);
-        return SLASH_STAR;
+        return IN_COMMENT;
     } else {
-        return SLASH_STAR;
+        return IN_COMMENT;
     }
 }
 
-enum Statetype handleStringState(int c){
+enum Statetype handleInStringState(int c){
     if(c == '"'){
         putchar(c);
-        return NOT_COMMENT;
+        return NOT_IN_COMMENT;
     } else if(c == '\\'){
         putchar(c);
-        return BACKSLASH_STRING;
+        return ESCAPE_IN_STRING;
     } else {
         putchar(c);
-        return STRING;
+        return IN_STRING;
     }
 }
 
-enum Statetype handleBackslashStringState(int c){
+enum Statetype handleEscapeInStringState(int c){
     putchar(c);
-    return STRING;
+    return IN_STRING;
 }
 
-enum Statetype handleBackslashCharState(int c){
+enum Statetype handleEscapeInCharState(int c){
     putchar(c);
-    return CHAR;
+    return IN_CHAR;
 }
 
-enum Statetype handleCharState(int c){
+enum Statetype handleInCharState(int c){
     if(c == '\''){
         putchar(c);
-        return NOT_COMMENT;
+        return NOT_IN_COMMENT;
     } else if(c == '\\'){
         putchar(c);
-        return BACKSLASH_CHAR;
+        return ESCAPE_IN_CHAR;
     } else {
         putchar(c);
-        return CHAR;
+        return IN_CHAR;
     }
 }
 
@@ -106,40 +106,40 @@ int main(void){
     int linenum = LINE_START;
     int errorline = LINE_START;
     int status = EXIT_SUCCESS;
-    enum Statetype state = NOT_COMMENT;
+    enum Statetype state = NOT_IN_COMMENT;
 
     while((c = getchar()) != EOF){
         switch(state){
-            case NOT_COMMENT:
-                state = handleNotCommentState(c);
+            case NOT_IN_COMMENT:
+                state = handleNotInCommentState(c);
                 status = EXIT_SUCCESS;
                 break;
             case SLASH:
                 state = handleSlashState(c);
                 status = EXIT_SUCCESS;
                 break;
-            case SLASH_STAR:
-                state = handleSlashStarState(c);
+            case IN_COMMENT:
+                state = handleInCommentState(c);
                 status = EXIT_FAILURE;
                 break;
-            case SLASH_STAR_STAR:
-                state = handleSlashStarStarState(c);
+            case IN_COMMENT_STAR:
+                state = handleInCommentStarState(c);
                 status = EXIT_FAILURE;
                 break;
-            case STRING:
-                state = handleStringState(c);
+            case IN_STRING:
+                state = handleInStringState(c);
                 status = EXIT_SUCCESS;
                 break;
-            case BACKSLASH_STRING:
-                state = handleBackslashStringState(c);
+            case ESCAPE_IN_STRING:
+                state = handleEscapeInStringState(c);
                 status = EXIT_SUCCESS;
                 break;
-            case BACKSLASH_CHAR:
-                state = handleBackslashCharState(c);
+            case ESCAPE_IN_CHAR:
+                state = handleEscapeInCharState(c);
                 status = EXIT_SUCCESS;
                 break;
-            case CHAR:
-                state = handleCharState(c);
+            case IN_CHAR:
+                state = handleInCharState(c);
                 status = EXIT_SUCCESS;
                 break;
         }
